192fb208d4c45dfd8ab1c7174a13d7551005dded
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-06-07 09:55:44 -0700

    Create TestData.js

---
+++ TestData.js
@@ -0,0 +1,34 @@
+[
+  {
+    "BridgeOpeningId": 502460,
+    "EventText": "On SR 99 northbound & southbound at 1st Ave S Bridge (MP 26) all lanes will be closed today at 9:02 AM due to boat traffic.  Drivers should anticipate a delay of up to 15 minutes or consider using an alternate route.",
+    "Status": "",
+    "OriginalBridgeOpenScheduleDate": null,
+    "Duration": 0,
+    "OpeningTime": "",
+    "BridgeLocation": {
+      "Description": "1st Avenue South Bridge",
+      "Direction": "B",
+      "RoadName": "099",
+      "Longitude": -122.331524751,
+      "Latitude": 47.5337063,
+      "MilePost": 26
+    }
+  },
+  {
+    "BridgeOpeningId": 52070,
+    "BridgeLocation": {
+      "Description": "Hood Canal",
+      "RoadName": "104        ",
+      "Direction": "Both",
+      "MilePost": 14.47,
+      "Latitude": 47.8584,
+      "Longitude": -122.6216
+    },
+    "Status": "Scheduled",
+    "OpeningTime": "2022-06-07T14:15:00",
+    "OriginalBridgeOpenScheduleDate": null,
+    "Duration": 60,
+    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 2:15 pm on Tuesday June 7 for boat traffic. Drivers can expect delays of up to 60 minutes."
+  }
+]
