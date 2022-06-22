4362ab794ca2f2e08eba5a78bd0b4701d0a3861b
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-06-22 08:34:22 -0700

    Update TestData.js

--- TestData.js
+++ TestData.js
@@ -2,7 +2,7 @@
 "startDate": "2022-01-01",
 "endDate": "2022-12-31",
 "themeId": 3,
-"bannerText": "Test Alert",
-"title": "Test Alert",
-"details": "Test Alert"
+"bannerText": "Test Alert 2",
+"title": "Test Alert 2",
+"details": "Test Alert 2"
 }
