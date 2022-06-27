abf3750563f88c086697194d195d4a422dd0261c
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-06-27 10:44:41 -0700

    Create travelCharts.js

---
+++ travelCharts.js
@@ -0,0 +1,165 @@
+{
+  "available": true,
+  "name": "Best times to travel: 4th of July weekend travel times",
+  "routes": [
+    {
+      "name": "I-5 Lacey to Tacoma",
+      "charts": [
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Thur.png?itok=spq2hlVn",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Fri.png?itok=N09nFNcy",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Sat.png?itok=1LPJ77IR",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Sun.png?itok=gcyFtA0-",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Mon.png?itok=3k_8ekDR",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_NB-Tue.png?itok=Wi1vKm_T",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Thur%20.png?itok=vxKHxC4J",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Fri%20.png?itok=qWozk51t",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Sat.png?itok=ossSK98r",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Sun.png?itok=DPGqcvtN",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Mon.png?itok=-StG6sxf",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-5_SB-Tue%20.png?itok=7VGu5ZTt",
+          "altText": "2022 4th of July travel chart"
+        }
+      ]
+    },
+    {
+      "name": "I-90 North Bend to Cle Elum",
+      "charts": [
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Thur.png?itok=qD64Wnlk",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Fri.png?itok=rtBHupNW",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Sat.png?itok=WDzp0Ro7",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Sun.png?itok=Yt9a3G46",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Mon.png?itok=Ve2UKeBB",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_EB-Tue.png?itok=S9vYgD_5",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Thur%20.png?itok=GfSQZ5mN",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Fri%20.png?itok=7XSWSJGE",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Sat.png?itok=3NzQso_1",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Sun.png?itok=sxTkTwJW",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Mon.png?itok=mYp8Oe_4",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/I-90_SB-Tue%20.png?itok=54PSd6-d",
+          "altText": "2022 4th of July travel chart"
+        }
+      ]
+    },
+    {
+      "name": "US 2 between Stevens Pass and Skykomish",
+      "charts": [
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Thur.png?itok=HpJ1IHhz",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Fri.png?itok=EjIkHoW5",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Sat.png?itok=W-Gm1RRv",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Sun.png?itok=8JFdSuk8",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Mon.png?itok=on_9lxa7",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_EB-Tue.png?itok=Sj7jq6RD",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Thur%20.png?itok=9xoWFH1W",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Fri%20.png?itok=MC9Gr3Bb",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Sat.png?itok=dBisIdIq",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Sun.png?itok=ALEBP7HF",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Mon.png?itok=Ldp4ZkGg",
+          "altText": "2022 4th of July travel chart"
+        },
+        {
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-06/US2_WB-Tue%20.png?itok=1Qam1Sic",
+          "altText": "2022 4th of July travel chart"
+        }
+      ]
+    }
+  ]
+}
