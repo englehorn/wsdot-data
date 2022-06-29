bb53132d780d1b066349e43b37069d526a2534ba
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-06-29 13:33:27 -0700

    Update EventStatus.js

--- EventStatus.js
+++ EventStatus.js
@@ -4,5 +4,5 @@
 "themeId": 4,
 "bannerText": "This is a Test Emergency Alert",
 "title": "Emergency Alert",
-"details": "Test Alert: <a href=\"https://wsdot.wa.gov/\">WSDOT Website</a>"
+"details": "The Washington State Department of Transportation provides and supports safe, reliable and cost-effective transportation options to improve livable communities and economic vitality for people and businesses.\n\nThe WSDOT mobile app was created to make it easier for you to know the latest about Washington's transportation system. \n\nQuestions, comments or suggestions about this app can be e-mailed to the WSDOT Communications Office at webfeedback@wsdot.wa.gov. \n"
 }
