835a5930002d87567ba7a5936f0f881f7c731c43
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-01 14:53:07 -0700

    Create BorderCrossings.js

---
+++ BorderCrossings.js
@@ -0,0 +1,186 @@
+{
+"waittimes": {
+"items": [
+{
+"wait": 2,
+"route": 5,
+"id": 100,
+"name": "Peace Arch",
+"lane": "Car",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 0,
+"route": 5,
+"id": 101,
+"name": "Peace Arch",
+"lane": "Nexus",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 60,
+"route": 5,
+"id": 102,
+"name": "Peace Arch",
+"lane": "Car",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 5,
+"route": 5,
+"id": 103,
+"name": "Peace Arch",
+"lane": "Nexus",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 2,
+"route": 543,
+"id": 104,
+"name": "Pacific Highway",
+"lane": "Car",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 0,
+"route": 543,
+"id": 105,
+"name": "Pacific Highway",
+"lane": "Fast",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 0,
+"route": 543,
+"id": 106,
+"name": "Pacific Highway",
+"lane": "Nexus",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 0,
+"route": 543,
+"id": 107,
+"name": "Pacific Highway",
+"lane": "Truck",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": 30,
+"route": 543,
+"id": 108,
+"name": "Pacific Highway",
+"lane": "Car",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 5,
+"route": 543,
+"id": 109,
+"name": "Pacific Highway",
+"lane": "Fast",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 15,
+"route": 543,
+"id": 110,
+"name": "Pacific Highway",
+"lane": "Nexus",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 5,
+"route": 543,
+"id": 111,
+"name": "Pacific Highway",
+"lane": "Truck",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": -1,
+"route": 539,
+"id": 112,
+"name": "Lynden/Aldergrove",
+"lane": "Car",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": -1,
+"route": 539,
+"id": 113,
+"name": "Lynden/Aldergrove",
+"lane": "Nexus",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": -1,
+"route": 539,
+"id": 114,
+"name": "Lynden/Aldergrove",
+"lane": "Car",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": -1,
+"route": 539,
+"id": 115,
+"name": "Lynden/Aldergrove",
+"lane": "Nexus",
+"direction": "southbound",
+"updated": "Not Available"
+},
+{
+"wait": 5,
+"route": 9,
+"id": 116,
+"name": "Sumas/Huntingdon",
+"lane": "Car",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 5,
+"route": 9,
+"id": 117,
+"name": "Sumas/Huntingdon",
+"lane": "Nexus",
+"direction": "northbound",
+"updated": "2022-07-01 02:45 PM"
+},
+{
+"wait": 4,
+"route": 9,
+"id": 118,
+"name": "Sumas/Huntingdon",
+"lane": "Car",
+"direction": "southbound",
+"updated": "2022-07-01 02:40 PM"
+},
+{
+"wait": -1,
+"route": 9,
+"id": 119,
+"name": "Sumas/Huntingdon",
+"lane": "Nexus",
+"direction": "southbound",
+"updated": "Not Available"
+}
+]
+}
+}
