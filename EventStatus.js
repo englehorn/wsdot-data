e47ea2a73fd8087da34613145266b0e3b5c8d84b
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-07 10:58:40 -0700

    Update EventStatus.js

--- EventStatus.js
+++ EventStatus.js
@@ -1,8 +1,8 @@
 {
-"startDate": "2019-04-08",
-"endDate": "2019-04-12",
-"themeId": 1,
-"bannerText": "We've gone orange to raise awareness for workzone safety",
-"title": "Workzone Awareness Week",
-"details": "We've gone orange to raise awareness for workzone safety https://www.wsdot.wa.gov/safety/brake/"
+"startDate": "2022-01-01",
+"endDate": "2022-12-01",
+"themeId": 2,
+"bannerText": "Test",
+"title": "Test",
+"details": "Test"
 }
