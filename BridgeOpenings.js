7b5aeb1db9888701c2a993afc467c283dc9da8a2
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-13 16:37:08 -0700

    Create BridgeOpenings.js

---
+++ BridgeOpenings.js
@@ -0,0 +1,18 @@
+[
+{
+"EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 10:30 am on Monday June 27 for maintenance. Drivers can expect delays of up to 30 minutes.",
+"Status": "Scheduled",
+"OpeningTime": "2022-06-27T10:30:00",
+"BridgeOpeningId": 52101,
+"BridgeLocation": {
+"Description": "Hood Canal",
+"Longitude": -122.6216,
+"Direction": "Both",
+"Latitude": 47.8584,
+"RoadName": "104        ",
+"MilePost": 14.47
+},
+"Duration": 30,
+"OriginalBridgeOpenScheduleDate": null
+}
+]
