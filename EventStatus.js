e94007036d664e020915d42503ed70109d98b7e1
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-20 10:52:51 -0700

    Update EventStatus.js

--- EventStatus.js
+++ EventStatus.js
@@ -1,8 +1,8 @@
 {
 "startDate": "2022-01-01",
-"endDate": "2022-12-01",
-"themeId": 2,
-"bannerText": "Test",
-"title": "Test",
-"details": "Whether you’re commuting, road-tripping or driving commercially, we have information to help you plan your route. See real-time alerts, live cameras, current travel times, and bridge and road restrictions for cars and trucks."
+"endDate": "2022-12-31",
+"themeId": 4,
+"bannerText": "This is a Test Emergency Alert",
+"title": "Emergency Alert",
+"details": "Test Alert: <a href=\"https://wsdot.wa.gov/\">WSDOT Website</a>"
 }
