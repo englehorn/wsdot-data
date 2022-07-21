9a07b5c96bc06f0f8fcb828b8ed7d90a7e36cc93
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-21 14:46:59 -0700

    Create NotificationTopics.js

---
+++ NotificationTopics.js
@@ -0,0 +1,274 @@
+{
+"topics": [
+{
+"topic": "interstate_bridge",
+"title": "Interstate Bridge",
+"category": "Bridge Alerts"
+},
+{
+"topic": "1st_ave_bridge",
+"title": "1st Avenue South Bridge in Seattle",
+"category": "Bridge Alerts"
+},
+{
+"topic": "hood_canal_bridge",
+"title": "Hood Canal Bridge Openings",
+"category": "Bridge Alerts"
+},
+{
+"topic": "ferry_route_9",
+"title": "Anacortes / San Juan Islands",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_272",
+"title": "Anacortes / San Juan Islands / Sidney B.C.",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_6",
+"title": "Edmonds / Kingston",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_14",
+"title": "Fauntleroy (West Seattle) / Vashon",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_13",
+"title": "Fauntleroy (West Seattle) / Southworth",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_300",
+"title": "Keller Ferry",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_7",
+"title": "Mukilteo / Clinton",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_8",
+"title": "Port Townsend / Coupeville",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_1",
+"title": "Pt Defiance / Tahlequah",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_5",
+"title": "Seattle / Bainbridge Island",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_3",
+"title": "Seattle / Bremerton",
+"category": "Ferry Alerts"
+},
+{
+"topic": "ferry_route_15",
+"title": "Southworth / Vashon",
+"category": "Ferry Alerts"
+},
+{
+"topic": "adams_county",
+"title": "Adams County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "asotin_county",
+"title": "Asotin County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "benton_county",
+"title": "Benton County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "chelan_county",
+"title": "Chelan County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "clallam_county",
+"title": "Clallam County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "clark_county",
+"title": "Clark County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "columbia_county",
+"title": "Columbia County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "cowlitz_county",
+"title": "Cowlitz County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "douglas_county",
+"title": "Douglas County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "ferry_county",
+"title": "Ferry County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "franklin_county",
+"title": "Franklin County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "garfield_county",
+"title": "Garfield County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "grant_county",
+"title": "Grant County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "grays_harbor_county",
+"title": "Grays Harbor County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "island_county",
+"title": "Island County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "jefferson_county",
+"title": "Jefferson County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "king_county",
+"title": "King County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "kitsap_county",
+"title": "Kitsap County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "kittitas_county",
+"title": "Kittitas County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "klickitat_county",
+"title": "Klickitat County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "lewis_county",
+"title": "Lewis County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "lincoln_county",
+"title": "Lincoln County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "mason_county",
+"title": "Mason County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "okanogan_county",
+"title": "Okanogan County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "pacific_county",
+"title": "Pacific County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "pend_oreille_county",
+"title": "Pend Oreille County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "pierce_county",
+"title": "Pierce County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "san_juan_county",
+"title": "San Juan County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "skagit_county",
+"title": "Skagit County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "skamania_county",
+"title": "Skamania County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "snohomish_county",
+"title": "Snohomish County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "spokane_county",
+"title": "Spokane County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "stevens_county",
+"title": "Stevens County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "thurston_county",
+"title": "Thurston County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "wahkiakum_county",
+"title": "Wahkiakum County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "walla_walla_county",
+"title": "Walla Walla County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "whatcom_county",
+"title": "Whatcom County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "whitman_county",
+"title": "Whitman County",
+"category": "Traffic Alerts"
+},
+{
+"topic": "yakima_county",
+"title": "Yakima County",
+"category": "Traffic Alerts"
+}
+]
+}
