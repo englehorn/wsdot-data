cc930503ffe9048e5f3273206ee47fa682d14127
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-07-25 14:28:23 -0700

    Update EventStatus.js

--- EventStatus.js
+++ EventStatus.js
@@ -1,6 +1,6 @@
 {
-"startDate": "2021-01-01",
-"endDate": "2021-12-31",
+"startDate": "2022-01-01",
+"endDate": "2022-12-31",
 "themeId": 4,
 "bannerText": "This is a Test Emergency Alert",
 "title": "Emergency Alert",
