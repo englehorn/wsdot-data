8813beb1272f7237a06950077ed09f7584a7f65d
Andrew Englehorn <engleha@wsdot.wa.gov>
2022-12-13 09:34:51 -0800

    Update StaticTollRates.js

--- StaticTollRates.js
+++ StaticTollRates.js
@@ -15,84 +15,84 @@
               "start_time": "0:00",
               "end_time": "4:59",
               "weekday": true,
-              "rows": ["Midnight to 5 AM", "1.25", "3.25"]
+              "rows": ["Midnight to 5 AM", "$1.25", "$3.25"]
             },
             {
               "header": false,
               "start_time": "5:00",
               "end_time": "5:59",   
               "weekday": true,
-              "rows": ["5 AM to 6 AM", "2.00", "4.00"]
+              "rows": ["5 AM to 6 AM", "$2.00", "$4.00"]
             },
             {
               "header": false,
               "start_time": "6:00",
               "end_time": "6:59", 
               "weekday": true,
-              "rows": ["6 AM to 7 AM", "3.40", "5.40"]
+              "rows": ["6 AM to 7 AM", "$3.40", "$5.40"]
             },
             { 
               "header": false,
               "start_time": "7:00",
               "end_time": "8:59", 
               "weekday": true,
-              "rows": ["7 AM to 9 AM", "4.30", "6.30"]
+              "rows": ["7 AM to 9 AM", "$4.30", "$6.30"]
             },
             {
               "header": false,
               "start_time": "9:00",
               "end_time": "9:59", 
               "weekday": true,
-              "rows": ["9 AM to 10 AM", "3.40", "5.40"]
+              "rows": ["9 AM to 10 AM", "$3.40", "$5.40"]
             },
             {
               "header": false,
               "start_time": "10:00",
               "end_time": "13:59", 
               "weekday": true,
-              "rows": ["10 AM to 2 PM", "2.70", "4.70"]
+              "rows": ["10 AM to 2 PM", "$2.70", "$4.70"]
             },
             {
               "header": false,
               "start_time": "14:00",
               "end_time": "14:59", 
               "weekday": true,
-              "rows": ["2 PM to 3 PM", "3.40", "5.40"]
+              "rows": ["2 PM to 3 PM", "$3.40", "$5.40"]
             },
             {
               "header": false,
               "start_time": "15:00",
               "end_time": "17:59", 
               "weekday": true,
-              "rows": ["3 PM to 6 PM", "4.30", "6.30"]
+              "rows": ["3 PM to 6 PM", "$4.30", "$6.30"]
             },
             {
               "header": false,
               "start_time": "18:00",
               "end_time": "18:59", 
               "weekday": true,
-              "rows": ["6 PM to 7 PM", "3.40", "5.40"]
+              "rows": ["6 PM to 7 PM", "$3.40", "$5.40"]
             },
             {
               "header": false,
               "start_time": "19:00",
               "end_time": "20:59", 
               "weekday": true,
-              "rows": ["7 PM to 9 PM", "2.70", "4.70"]
+              "rows": ["7 PM to 9 PM", "$2.70", "$4.70"]
             },
             {
               "header": false,
               "start_time": "21:00",
               "end_time": "22:59", 
               "weekday": true,
-              "rows": ["9 PM to 11 PM", "2.00", "4.00"]
+              "rows": ["9 PM to 11 PM", "$2.00", "$4.00"]
             },
             {
               "header": false,
               "start_time": "23:00",
               "end_time": "23:59", 
               "weekday": true,
-              "rows": ["11 PM to 11:59 PM", "1.25", "3.25"]
+              "rows": ["11 PM to 11:59 PM", "$1.25", "$3.25"]
             },
             {
               "header": true,
@@ -104,49 +104,49 @@
               "start_time": "0:00",
               "end_time": "4:59", 
               "weekday": false,
-              "rows": ["Midnight to 5 AM", "1.25", "3.25"]
+              "rows": ["Midnight to 5 AM", "$1.25", "$3.25"]
 	    },
 	    {
               "header": false,
               "start_time": "5:00",
               "end_time": "7:59", 
               "weekday": false,
-              "rows": ["5 AM to 8 AM", "1.40", "3.40"]
+              "rows": ["5 AM to 8 AM", "$1.40", "$3.40"]
             },
             {
               "header": false,
               "start_time": "8:00",
               "end_time": "10:59", 
               "weekday": false,
-              "rows": ["8 AM to 11 AM", "2.05", "4.05"]
+              "rows": ["8 AM to 11 AM", "$2.05", "$4.05"]
             },
             {
               "header": false,
               "start_time": "11:00",
               "end_time": "17:59", 
               "weekday": false,
-              "rows": ["11 AM to 6 PM", "2.65", "4.65"]
+              "rows": ["11 AM to 6 PM", "$2.65", "$4.65"]
             },
             {
               "header": false,
               "start_time": "18:00",
               "end_time": "20:59", 
               "weekday": false,
-              "rows": ["6 PM to 9 PM", "2.05", "4.05"]
+              "rows": ["6 PM to 9 PM", "$2.05", "$4.05"]
             },
             {
               "header": false,
               "start_time": "21:00",
               "end_time": "22:59", 
               "weekday": false,
-              "rows": ["9 PM to 11 PM", "1.40", "3.40"]
+              "rows": ["9 PM to 11 PM", "$1.40", "$3.40"]
             },
             {
               "header": false,
               "start_time": "23:00",
               "end_time": "23:59", 
               "weekday": false,
-              "rows": ["11 PM to 11:59 PM", "1.25", "3.25"]
+              "rows": ["11 PM to 11:59 PM", "$1.25", "$3.25"]
             }
           ]
        },
@@ -163,32 +163,32 @@
             {
               "header": false,
               "weekday": true,
-              "rows": ["6 AM to 7 AM", "1.50", "3.50"] 
+              "rows": ["6 AM to 7 AM", "$1.50", "$3.50"] 
             },
             {
               "header": false,
               "weekday": true,
-              "rows": ["7 AM to 9 AM", "1.80", "3.80"] 
+              "rows": ["7 AM to 9 AM", "$1.80", "$3.80"] 
             },
             {
               "header": false,
               "weekday": true,
-              "rows": ["9 AM to 3 PM", "1.50", "3.50"] 
+              "rows": ["9 AM to 3 PM", "$1.50", "$3.50"] 
             },
             {
               "header": false,
               "weekday": true,
-              "rows": ["3 PM to 6 PM", "2.70", "4.70"] 
+              "rows": ["3 PM to 6 PM", "$2.70", "$4.70"] 
             },
             {
               "header": false,
               "weekday": true,
-              "rows": ["6 PM to 11 PM", "1.50", "3.50"] 
+              "rows": ["6 PM to 11 PM", "$1.50", "$3.50"] 
             },
             {
               "header": false,
               "weekday": true,
-              "rows": ["11 PM to 6 AM", "1.20", "3.20"] 
+              "rows": ["11 PM to 6 AM", "$1.20", "$3.20"] 
             },
             {
               "header": true,
@@ -198,7 +198,7 @@
             {
               "header": false,
               "weekday": false,
-              "rows": ["All day", "1.20", "3.20"] 
+              "rows": ["All day", "$1.20", "$3.20"] 
             }
           ]
        },
@@ -213,23 +213,23 @@
             },
             {
               "header": false,
-              "rows": ["Two (includes motorcycle)", "5.25", "6.25", "7.25"]
+              "rows": ["Two (includes motorcycle)", "$4.50", "$5.50", "$6.50"]
             },
             {
               "header": false,
-              "rows": ["Three", "7.90", "9.40", "10.90"]
+              "rows": ["Three", "$6.75", "$8.25", "$9.75"]
             },
             {
               "header": false,
-              "rows": ["Four", "10.50", "12.50", "14.50"]
+              "rows": ["Four", "$9.00", "$11.00", "$13.00"]
             },
             {
               "header": false,
-              "rows": ["Five", "13.15", "15.65", "18.15"]
+              "rows": ["Five", "$11.25", "$13.75", "$16.25"]
             },
             {
               "header": false,
-              "rows": ["Six or more", "15.75", "18.75", "21.75"]
+              "rows": ["Six or more", "$13.50", "$16.50", "$19.50"]
             }
           ]
        }
