6ccb582dd8d96bb4b9ca80758668a08d2f049f77
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-02-15 10:14:28 -0800

    Update BridgeOpenings.js

--- BridgeOpenings.js
+++ BridgeOpenings.js
@@ -1,6 +1,6 @@
 [
 {
-"EventText": "EventText1",
+"EventText": "Text1",
 "Status": "",
 "OpeningTime": "2022-06-27T10:30:00",
 "BridgeOpeningId": 52101,
@@ -16,12 +16,12 @@
 "OriginalBridgeOpenScheduleDate": null
 },
   {
-"EventText": "EventText2",
+"EventText": "Text2",
 "Status": "",
 "OpeningTime": "2022-06-27T10:30:00",
 "BridgeOpeningId": 52102,
 "BridgeLocation": {
-"Description": "Hood Canal 2",
+"Description": "Hood Canal",
 "Longitude": -122.6216,
 "Direction": "Both",
 "Latitude": 47.8584,
@@ -32,12 +32,44 @@
 "OriginalBridgeOpenScheduleDate": null
 },
   {
-"EventText": "EventText3",
+"EventText": "Text1",
 "Status": "",
 "OpeningTime": "2022-06-27T10:30:00",
 "BridgeOpeningId": 52103,
 "BridgeLocation": {
-"Description": "Hood Canal 3",
+"Description": "1st Avenue South Bridge",
+"Longitude": -122.6216,
+"Direction": "Both",
+"Latitude": 47.8584,
+"RoadName": "104        ",
+"MilePost": 14.47
+},
+"Duration": 30,
+"OriginalBridgeOpenScheduleDate": null
+},
+    {
+"EventText": "Text1",
+"Status": "",
+"OpeningTime": "2022-06-27T10:30:00",
+"BridgeOpeningId": 52103,
+"BridgeLocation": {
+"Description": "Interstate Bridge",
+"Longitude": -122.6216,
+"Direction": "Both",
+"Latitude": 47.8584,
+"RoadName": "104        ",
+"MilePost": 14.47
+},
+"Duration": 30,
+"OriginalBridgeOpenScheduleDate": null
+},
+      {
+"EventText": "Text2",
+"Status": "",
+"OpeningTime": "2022-06-27T10:30:00",
+"BridgeOpeningId": 52103,
+"BridgeLocation": {
+"Description": "Interstate Bridge",
 "Longitude": -122.6216,
 "Direction": "Both",
 "Latitude": 47.8584,
