2ba606b0c628a29068a8284d0ca45ff011993178
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-05-22 16:07:52 -0700

    Update travelCharts.js

--- travelCharts.js
+++ travelCharts.js
@@ -1,57 +1,57 @@
 {
   "available": true,
-  "name": "Best times to travel: Labor Day weekend travel times",
+  "name": "Best times to travel: Memorial Day weekend travel times",
   "routes": [
     {
       "name": "I-5 Lacey to Tacoma",
       "charts": [
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Thur.png?itok=eKbCQM7i",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Thu.png?itok=b7yv3Yz7",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Fri.png?itok=dDlCv5Mg",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Fri.png?itok=yujlX_qJ",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Sat.png?itok=GcgTMLyw",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Sat.png?itok=F-pmGI4b",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Sun.png?itok=_1qQVFJu",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Sun.png?itok=cCKRdnpE",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Mon.png?itok=zX2Dj1i_",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Mon.png?itok=6uIsbQJh",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_NB-Tue.png?itok=j7cqhiuC",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_NB-Tu.png?itok=Q88bo7aV",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Thur.png?itok=LwSHGdYb",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Thu.png?itok=QcC8gqMV",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Fri.png?itok=mafmKQPz",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Fri.png?itok=WQOllmP2",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Sat.png?itok=9WJdSXCO",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Sat.png?itok=A30uyC2K",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Sun.png?itok=gpECMXpM",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Sun.png?itok=W2BmVr2v",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Mon.png?itok=UXlbxaI4",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Mon.png?itok=_PehjIev",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-5_SB-Tue.png?itok=6EISmP9k",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-5_SB-Tu.png?itok=SEI54Q2S",
+          "altText": "2023 Memorial Day travel chart"
         }
       ]
     },
@@ -59,52 +59,52 @@
       "name": "I-90 North Bend to Cle Elum",
       "charts": [
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Thur_0.png?itok=iBr1bl0k",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Thu.png?itok=yzHu_5B2",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Fri.png?itok=Y_3wUTBM",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Fir.png?itok=lEkYtntA",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Sat.png?itok=J1NQLLYd",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Sat.png?itok=myLAjIWQ",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Sun.png?itok=1bgaHpMR",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Sun.png?itok=ATG5VlWr",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Mon.png?itok=geSoOqth",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Mon.png?itok=dsew3Hbf",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_EB-Tue.png?itok=SWvTe2JD",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_EB-Tu.png?itok=RjPlo_ik",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Thur.png?itok=SX-680mU",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Thu.png?itok=GkYDTa3Y",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Fri.png?itok=7NIN-VkD",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Fri.png?itok=d9Av0n_I",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Sat.png?itok=jAR4nV7-",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Sat.png?itok=-eTEYhEF",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Sun.png?itok=0m498LFI",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Sun.png?itok=hpi-tYwN",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Mon.png?itok=0Y4sPQ9J",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Mon.png?itok=uuBLea3l",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-I-90_WB-Tue.png?itok=nc-1iWLT",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-I-90_WB-Tu.png?itok=eaFrXP8r",
+          "altText": "2023 Memorial Day travel chart"
         }
       ]
     },
@@ -112,52 +112,52 @@
       "name": "US 2 between Stevens Pass and Skykomish",
       "charts": [
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Thur.png?itok=Z91iWjtQ",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Thu.png?itok=IdQ82Oxb",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Fri.png?itok=WR53UbKi",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Fri.png?itok=ytX4KhpP",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Sat.png?itok=nMWSK9Ov",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Sat.png?itok=ICmwJqvB",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Sun.png?itok=_3oyX4fP",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Sun.png?itok=hBXcp_ZL",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Mon.png?itok=7VvvuvUx",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Mon.png?itok=jNt_kauc",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_EB-Tue.png?itok=NIF-jpWY",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_EB-Tu.png?itok=TcGiXJMM",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Thur.png?itok=fuqwrrNm",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Thu.png?itok=tO9ZaSu_",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Fri%20png.png?itok=2DwCTZi0",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Fri.png?itok=UYRfeI_H",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Sat.png?itok=8Vdk98uT",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Sat.png?itok=xRpaowwi",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Sun.png?itok=lajy9Xc7",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Sun.png?itok=4ENMER31",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Mon.png?itok=B2jZigg5",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Mon.png?itok=QQHrwybY",
+          "altText": "2023 Memorial Day travel chart"
         },
         {
-          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2022-08/labor-day-2022-US2_WB-Tue.png?itok=jqxb1QPO",
-          "altText": "2022 Labor Day travel chart"
+          "url": "https://wsdot.wa.gov/sites/default/files/styles/max_650x650/public/2023-05/memorial-day-2023-US2_WB-Tu.png?itok=CLhp2zgy",
+          "altText": "2023 Memorial Day travel chart"
         }
       ]
     }
