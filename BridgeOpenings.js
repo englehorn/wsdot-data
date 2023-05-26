7296109b0a7e05ae852a00632c977b43bcd4d08b
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-05-26 16:28:14 -0700

    Update BridgeOpenings.js

--- BridgeOpenings.js
+++ BridgeOpenings.js
@@ -1,24 +1,6 @@
 [
   {
-    "BridgeOpeningId": 551698,
-    "EventText": "On SR 99 northbound & southbound at 1st Ave S Bridge (MP 26) all lanes will be closed today at 11:34 AM due to boat traffic.  Drivers should anticipate a delay of up to 15 minutes or consider using an alternate route.",
-    "Status": "",
-    "OriginalBridgeOpenScheduleDate": null,
-    "Duration": 0,
-    "OpeningTime": "",
-    "Priority": "Highest",
-    "EventCategory": "Boat Traffic",
-    "BridgeLocation": {
-      "Description": "1st Avenue South Bridge",
-      "Direction": "B",
-      "RoadName": "099",
-      "Longitude": -122.331524751,
-      "Latitude": 47.5337063,
-      "MilePost": 26
-    }
-  },
-  {
-    "BridgeOpeningId": 62719,
+    "BridgeOpeningId": 62712,
     "BridgeLocation": {
       "Description": "Hood Canal",
       "RoadName": "104        ",
@@ -27,11 +9,30 @@
       "Latitude": 47.8584,
       "Longitude": -122.6216
     },
-    "EventCategory": "Boat Traffic",
     "Status": "Scheduled",
-    "OpeningTime": "2023-05-26T11:45:00",
+    "OpeningTime": "2023-05-27T13:45:00",
     "OriginalBridgeOpenScheduleDate": null,
     "Duration": 60,
-    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 11:45 am on Friday May 26 for boat traffic. Drivers can expect delays of up to 60 minutes."
+    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 1:45 pm on Saturday May 27 for boat traffic. Drivers can expect delays of up to 60 minutes.",
+    "Priority": "",
+    "EventCategory": ""
+  },
+  {
+    "BridgeOpeningId": 62718,
+    "BridgeLocation": {
+      "Description": "Hood Canal",
+      "RoadName": "104        ",
+      "Direction": "Both",
+      "MilePost": 14.47,
+      "Latitude": 47.8584,
+      "Longitude": -122.6216
+    },
+    "Status": "Scheduled",
+    "OpeningTime": "2023-05-27T12:45:00",
+    "OriginalBridgeOpenScheduleDate": null,
+    "Duration": 60,
+    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 12:45 pm on Saturday May 27 for boat traffic. Drivers can expect delays of up to 60 minutes.",
+    "Priority": "",
+    "EventCategory": ""
   }
 ]
