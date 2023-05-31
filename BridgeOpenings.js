4de2bee6a8974390e0e2ce927bf182f4bbb775f5
Andrew Englehorn <engleha@wsdot.wa.gov>
2023-05-31 16:38:44 -0700

    Update BridgeOpenings.js

--- BridgeOpenings.js
+++ BridgeOpenings.js
@@ -1,38 +1,13 @@
-[
-  {
-    "BridgeOpeningId": 62712,
-    "BridgeLocation": {
-      "Description": "Hood Canal",
-      "RoadName": "104        ",
-      "Direction": "Both",
-      "MilePost": 14.47,
-      "Latitude": 47.8584,
-      "Longitude": -122.6216
-    },
-    "Status": "Scheduled",
-    "OpeningTime": "2023-05-27T13:45:00",
-    "OriginalBridgeOpenScheduleDate": null,
-    "Duration": 60,
-    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 1:45 pm on Saturday May 27 for boat traffic. Drivers can expect delays of up to 60 minutes.",
-    "Priority": "Low",
-    "EventCategory": ""
-  },
-  {
-    "BridgeOpeningId": 62718,
-    "BridgeLocation": {
-      "Description": "Hood Canal",
-      "RoadName": "104        ",
-      "Direction": "Both",
-      "MilePost": 14.47,
-      "Latitude": 47.8584,
-      "Longitude": -122.6216
-    },
-    "Status": "Scheduled",
-    "OpeningTime": "2023-05-27T12:45:00",
-    "OriginalBridgeOpenScheduleDate": null,
-    "Duration": 60,
-    "EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 12:45 pm on Saturday May 27 for boat traffic. Drivers can expect delays of up to 60 minutes.",
-    "Priority": "",
-    "EventCategory": ""
-  }
-]
+[{
+	"EventText": "Text2",
+	"Status": "",
+	"OpeningTime": "2022-06-27T10:30:00",
+	"BridgeOpeningId": 52102,
+	"BridgeLocation": {
+		"Description": "Hood Canal",
+		"Longitude": -122.6216,
+		"Direction": "Both",
+		"Latitude": 47.8584,
+		"OriginalBridgeOpenScheduleDate": null
+	}
+}]
