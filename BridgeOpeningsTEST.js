39acc3b70754d48565b284ab1a2222b2e8fb7d3c
Andrew Englehorn <engleha@wsdot.wa.gov>
2024-09-24 08:33:02 -0700

    Add files via upload

---
+++ BridgeOpeningsTEST.js
@@ -0,0 +1,61 @@
+ [
+    {
+"EventText": "On SR 99 northbound & southbound at 1st Ave S Bridge (MP 26) all lanes will be closed today at 10:16 AM due to boat traffic.  Drivers should anticipate a delay of up to 15 minutes or consider using an alternate route.",
+"LastUpdatedTime": "/Date(1718212618597)/",
+"TravelCenterPriorityId": 2,
+"BridgeOpeningId": 600935,
+"EventCategoryType": "Blocking",
+"EventCategoryDescription": "Boat Traffic",
+"OriginalBridgeOpenScheduleDate": null,
+"EventCategoryTypeDescription": "Bridge",
+"Status": "",
+"BridgeLocation": {
+"Latitude": 47.5337063,
+"MilePost": 26,
+"Longitude": -122.331524751,
+"Description": "1st Avenue South Bridge",
+"Direction": "Both Directions",
+"RoadName": "SR 99"
+},
+"Duration": 0,
+"OpeningTime": ""
+},
+{
+"EventText": "On SR 104 a Hood Canal bridge closure has been scheduled for approximately 1:15 pm on Monday June 17 for boat traffic. Drivers can expect delays of up to 60 minutes.",
+"EventCategoryTypeDescription": "Bridge",
+"Status": "Scheduled",
+"OpeningTime": "2024-06-17T13:15:00",
+"OriginalBridgeOpenScheduleDate": null,
+"Duration": 60,
+"TravelCenterPriorityId": 2,
+"BridgeOpeningId": "684983",
+"BridgeLocation": {
+"Direction": "Both Directions",
+"Description": "Hood Canal",
+"Latitude": 47.8584,
+"MilePost": 14.47,
+"RoadName": "SR 104",
+"Longitude": -122.6216
+},
+"LastUpdatedTime": null
+},
+{
+"EventText": "Hood Canal Bridge closed, marine opening in progress. Motorists should expect delays for up to 60 minutes.",
+"EventCategoryTypeDescription": "Bridge",
+"Status": "Closed to traffic",
+"OpeningTime": "2024-06-17T08:15:00",
+"OriginalBridgeOpenScheduleDate": null,
+"Duration": 60,
+"TravelCenterPriorityId": 2,
+"BridgeOpeningId": "684971",
+"BridgeLocation": {
+"Direction": "Both Directions",
+"Description": "Hood Canal",
+"Latitude": 47.8584,
+"MilePost": 14.47,
+"RoadName": "SR 104",
+"Longitude": -122.6216
+},
+"LastUpdatedTime": null
+}
+ ]
